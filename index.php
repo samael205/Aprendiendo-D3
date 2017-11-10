cf4ddc3bf8c7dc136b1f20bd6d0e2aa825cfcb55
Pedro José <pedro5_crevi@hotmail.com>
2017-11-10 21:13:23 +0100

    Añadiendo Librerias
    Añado las librerias necesarias para trabajar con el proyecto

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="./js/popper.min.js"></script>
+	<script type="text/javascript" src="./js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/d3.min.js"></script>
+	<script type="text/javascript" src="./js/code.js"></script>
+	<link rel="stylesheet" href="./css/bootstrap.min.css">
+	<link rel="stylesheet" href="./css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="./css/bootstrap.reboot.min.css">
 	<title>Data Driven Documents</title>
 </head>
 <body>
